3b1a5f4badadd6160079f3539704bb4d64bd3940
picrap <pascal@craponne.fr>
2015-04-24 15:57:24 +0200

    Moved to Gracenote
    Which, surprisingly, works (opposed to MusicBrainz)

--- HeyMrDJ.Core/Lookup.cs
+++ HeyMrDJ.Core/Lookup.cs
@@ -9,73 +9,26 @@ namespace HeyMrDJ
     using System;
     using System.Collections.Generic;
     using System.Linq;
-    using System.Text;
     using DuoVia.FuzzyStrings;
-    using MusicBrainz;
+    using ParkSquare.Gracenote;
 
     public class Lookup
     {
         public void T()
         {
-            var t = FindTracks(FindAlbum(FindArtist("Didier Super"), "Bin quoi?"));
-            var t2 = FindTracks(FindAlbum(FindArtist("Aphrodite's child"), "666"));
-            var t3 = FindTracks(FindAlbum(FindArtist("Cyndi Lauper"), "Twelve deadly cyns"));
-            var t4 = FindTracks(FindAlbum(FindArtist("Cindy Lauper"), "Twelve deadly zinz"));
-            var t5 = FindTracks(FindAlbum(FindArtist("Madonna"), "Mozik"));
+            var c = new GracenoteClient("5911808-2E02E745CE9704D2BADA6060D461A3F8");
+            var r5 = c.Search(new SearchCriteria { Artist = "Madonna", AlbumTitle = "Music" });
+            var r1 = c.Search(new SearchCriteria { Artist = "Didier Super", AlbumTitle = "Bin quoi?" });
+            var r2 = c.Search(new SearchCriteria { Artist = "Aphrodite's child", AlbumTitle = "666" });
+            var r3 = c.Search(new SearchCriteria { Artist = "Cyndi Lauper", AlbumTitle = "Twelve deadly cyns" });
+            var r4 = c.Search(new SearchCriteria { Artist = "Cindy Lauper", AlbumTitle = "Twelve deadly zinz" });
 
-            var a = Search.Artist("Didier Super").Data[0];
-            var c = Search.Release(arid: a.Id);
-            var z = Search.Release("Didier Super - Ben quoi?");
-        }
+            //var t = FindTracks(FindAlbum(FindArtist("Didier Super"), "Bin quoi?"));
+            //var t2 = FindTracks(FindAlbum(FindArtist("Aphrodite's child"), "666"));
+            //var t3 = FindTracks(FindAlbum(FindArtist("Cyndi Lauper"), "Twelve deadly cyns"));
+            //var t4 = FindTracks(FindAlbum(FindArtist("Cindy Lauper"), "Twelve deadly zinz"));
+            //var t5 = FindTracks(FindAlbum(FindArtist("Madonna"), "Mozik"));
 
-        private string[][] FindTracks(IEnumerable<string> albumIDs)
-        {
-            return EnumerateTracks(albumIDs).ToArray();
-        }
-
-        private static IEnumerable<string[]> EnumerateTracks(IEnumerable<string> albumIDs)
-        {
-            foreach (var albumID in albumIDs)
-            {
-                var recording = Search.Recording(reid: albumID);
-                if (recording == null)
-                    continue;
-                yield return recording.Data.Select(t => t.Title).ToArray();
-            }
-        }
-
-        public string FindArtist(string name)
-        {
-            var artist = Search.Artist(Trim(name), limit: 1).Data.FirstOrDefault();
-            if (artist == null)
-                return null;
-            return artist.Id;
-        }
-
-        public string[] FindAlbum(string artistID, string name)
-        {
-            if (artistID == null)
-                return null;
-            var trimmedName = Trim(name);
-            var singleAlbum = Search.Release(trimmedName, arid: artistID, type: "album").Data
-                .Concat(Search.Release(trimmedName, arid: artistID, type: "compilation").Data)
-                .ToArray();
-            if (singleAlbum.Length == 0)
-            {
-                var dmNameWords = ToDoubleMetaphones(trimmedName);
-                var albums = Search.Release(arid: artistID, type: "album", limit: 100).Data.Concat(Search.Release(arid: artistID, type: "compilation", limit: 100).Data).ToArray();
-                var orderedAlbums = from album in albums
-                                    let dmTitleWords = ToDoubleMetaphones(Trim(album.Title))
-                                    let distance = LevenshteinDistance(dmTitleWords, dmNameWords)
-                                    group album by distance into homonyms
-                                    orderby homonyms.Key
-                                    select new { Distance = homonyms.Key, Albums = homonyms.ToArray() };
-                var firstAlbum = orderedAlbums.FirstOrDefault();
-                if (firstAlbum != null)
-                    return firstAlbum.Albums.Select(a => a.Id).ToArray();
-                return null;
-            }
-            return singleAlbum.Select(a => a.Id).ToArray();
         }
 
         private static string Trim(string rawName)
